eec17f0da2fdf5c23339059768c9e3e1aa9ecaae
PoupineX <92035497+PoupineX@users.noreply.github.com>
2022-10-17 00:18:35 +0200

    Create portal.py

---
+++ portal.py
@@ -0,0 +1,13 @@
+import pygame
+
+# classe representant les portails (6)
+class Portal(pygame.sprite.Sprite):
+
+    pos = [(1600, 810), (1600, 450), (1600, 90), (1100, 810), (1100, 450), (1100, 90)]
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.image.load("assets_NSI/portal_s.png")
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        #self.val = eliot.nbs_atr()

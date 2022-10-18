85bca1d6b2fab4c16e38cf9bc8c61977242aa609
PoupineX <92035497+PoupineX@users.noreply.github.com>
2022-10-19 00:52:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import math
 from game import Game
 
 pygame.init()
@@ -9,16 +10,13 @@ screen = pygame.display.set_mode(SIZE)
 # generer la fenêtre de jeu
 pygame.display.set_caption("Capybynara")
 
-
 # importer l'arriere-plan
 background1 = pygame.image.load("assets_NSI/bg1.png")
 
 # charger le jeu
 #if __name__ =="__main__":    
-game = Game()
+
 #game.run()
-
-
 
 
 # mettre le nombre a traduire en haut de l'ecran
@@ -26,7 +24,7 @@ font = pygame.font.SysFont("Liberation", 100)
 
 # creation les listes de propositions pour le QCM
 nbin_f = [16, 2, 3, 10, 8, 12, 15, 7, 4, 5, 6, 9, 11, 13]       # QCM facile
-nbin_m = [32, 40, 56, 17, 128, 64, 68]                          # QCM medium
+nbin_m = [32, 40, 56, 17, 127, 64, 68]                          # QCM medium
 nbin_d = [256, 175, 89, 243, 257, 512, 524, 532, 1024, 1064]    # QCM difficile
 
 # nbin1 est choisit aleatoirement dans la liste ( facile pour l'instant ), c'est la "bonne reponse"
@@ -39,7 +37,6 @@ nbin3 = random.choice(nbin_d)
 
 # Crée un affichage pour la bonne reponse a trouver en binaire
 questions = font.render(str(nbin1), 1, pygame.Color("White"))
-
 
 # Algorithme permettant de renvoyer les reponses du QCM
 def nbs_atr(reponse,n_bin_):
@@ -61,7 +58,6 @@ def nbs_atr(reponse,n_bin_):
         lcal_nbin.remove(nombre)
     random.shuffle(L)
     return L
-
 
 # liste des reponses QCM faciles
 L1 = nbs_atr(nbin1, nbin_f)
@@ -92,34 +88,54 @@ response4 = font.render(str(bin(L1[3])), 1, pygame.Color("Red"))    # ...4      
 response5 = font.render(str(bin(L1[4])), 1, pygame.Color("Red"))    # ...5      //
 response6 = font.render(str(bin(L1[5])), 1, pygame.Color("Red"))    # ...6      //
 
+######################################################### Essai ############################################################################
+#rectreponse1 = response1.get_rect()
 
+# ecran d'introduction au jeu
+intro = pygame.image.load("assets_NSI/capy.png")
+intro_rect = intro.get_rect()
+intro_rect.x = 0
+intro_rect.y = -140
+intro_txt = font.render("Welcome !", 1, pygame.Color("Green"))
+play_button = pygame.image.load("assets_NSI/start_btn.png")
+play_button = pygame.transform.scale(play_button, (400, 150))
+play_button_rect = play_button.get_rect()
+play_button_rect.x = math.ceil(WIDTH / 3 + 100)
+play_button_rect.y = math.ceil(HEIGHT / 2 - 100)
+
+game = Game()
 running = True
 clock = pygame.time.Clock()
 
 # boucle tant que cette condition est vraie
 while running:
-
+    
     # rafraichir a 60 images par seconde
     clock.tick(60)
 
     # appliquer l'arriere-plan
     screen.blit(background1, (0, -350))
 
-
     if game.is_playing:
-            game.update(screen)
+        game.update(screen)
 
-    # appliquer le nombre a trouver en haut de l'écran
-    screen.blit(questions, (WIDTH // 2 - 60, 20))
+        # appliquer le nombre a trouver en haut de l'écran
+        screen.blit(questions, (WIDTH // 2 - 60, 20))
 
-    # appliquer le QCM
-    screen.blit(response6, (game.portal_s6.rect))
-    screen.blit(response5, (game.portal_s5.rect))
-    screen.blit(response4, (game.portal_s4.rect))
-    screen.blit(response3, (game.portal_s3.rect))
-    screen.blit(response2, (game.portal_s2.rect))    
-    screen.blit(response1, (game.portal_s1.rect))
-    
+        # appliquer le QCM
+        screen.blit(response6, (game.portal_s6.rect))
+        screen.blit(response5, (game.portal_s5.rect))
+        screen.blit(response4, (game.portal_s4.rect))
+        screen.blit(response3, (game.portal_s3.rect))
+        screen.blit(response2, (game.portal_s2.rect))    
+        screen.blit(response1, (game.portal_s1.rect))
+    else:
+        # afficher ecran d'intro
+        screen.blit(intro, (intro_rect.x, intro_rect.y))
+        screen.blit(play_button, (play_button_rect.x, play_button_rect.y))
+        screen.blit(intro_txt, (WIDTH // 2 - 200, 20)) 
+
+
     # mettre a jour l'écran
     pygame.display.flip()
     #while not done:
@@ -132,5 +148,11 @@ while running:
             print("Le jeu est fermé") # A ENLEVER !!!!!!!!!!
         elif event.type == pygame.KEYDOWN:
             game.pressed[event.key] = True
+        
         elif event.type == pygame.KEYUP:
             game.pressed[event.key] = False
+        
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if play_button_rect.collidepoint(event.pos):
+                pygame.draw.rect(screen, (255, 0, 0), play_button_rect, 1)
+                game.is_playing = True

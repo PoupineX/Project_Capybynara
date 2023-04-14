60929c1bab91dec5efb87ac9c9ae62d806e66c91
PoupineX <92035497+PoupineX@users.noreply.github.com>
2023-04-14 16:22:05 +0200

    Update game.py
    V2 with health bar

--- game.py
+++ game.py
@@ -3,23 +3,24 @@ from player import Player
 from portal import Portal
 pygame.init() 
 
-
 info = pygame.display.Info()
 SIZE = WIDTH, HEIGHT = info.current_w - 60, info.current_h - 60
 font2 = pygame.font.SysFont("Liberation", 500)
 bravo = font2.render("BRAVO !!!", 1, pygame.Color("White"))
+gg = font2.render("Bonne réponse !", 0.8, pygame.Color("White"))
+
 class Game:
 
     def __init__(self):   # permet de creer des rectangles des objets et 
         self.is_playing = False
         self.player = Player()
         #self.all_portals = Portal(1600, 750) #and Portal(1600, 400)
-        self.portal_s1 = Portal(WIDTH - 300, HEIGHT - 250)
-        self.portal_s2 = Portal(WIDTH - 300, HEIGHT - 600)
-        self.portal_s3 = Portal(WIDTH - 300, HEIGHT - 950)
-        self.portal_s4 = Portal(WIDTH - 800, HEIGHT - 250)
-        self.portal_s5 = Portal(WIDTH - 800, HEIGHT - 600)
-        self.portal_s6 = Portal(WIDTH - 800, HEIGHT - 950)
+        self.portal_s1 = Portal(WIDTH - 300, HEIGHT - 250,False)
+        self.portal_s2 = Portal(WIDTH - 300, HEIGHT - 600,False)
+        self.portal_s3 = Portal(WIDTH - 300, HEIGHT - 950,False)
+        self.portal_s4 = Portal(WIDTH - 800, HEIGHT - 250,False)
+        self.portal_s5 = Portal(WIDTH - 800, HEIGHT - 600,False)
+        self.portal_s6 = Portal(WIDTH - 800, HEIGHT - 950,False)
         self.pressed = {}
 
 
@@ -27,6 +28,9 @@ class Game:
 
         # appliquer l'image du joueur a l'écran
         screen.blit(self.player.image, self.player.rect)
+
+        # mettre à jour les hp du joueur
+        self.player.update_hp_bar(screen)
 
         # appliquer les images du groupe de portails
         #game.portal.draw(screen)
@@ -51,35 +55,53 @@ class Game:
         if self.pressed.get(pygame.K_SPACE):
             self.is_playing = False
 
-        # collisions entre joueur et portails
+        # gestion des collisions entre joueur et portails
         if self.player.rect.colliderect(self.portal_s1.rect):
             pygame.draw.rect(screen, (255, 0, 0), self.player.rect, 1)
             pygame.draw.rect(screen, (255, 0, 0), self.portal_s1.rect, 1)
+            if self.portal_s1.reponse==True:
+                 screen.blit(bravo, (WIDTH // 2 - 850, 20))
+            else:
+                self.player.hp -= 2
+        
         if self.player.rect.colliderect(self.portal_s2.rect):
             pygame.draw.rect(screen, (255, 0, 0), self.player.rect, 1)
             pygame.draw.rect(screen, (255, 0, 0), self.portal_s2.rect, 1)
+            if self.portal_s2.reponse==True:
+                 screen.blit(bravo, (WIDTH // 2 - 850, 20))
+            else:
+                self.player.hp -= 2
+       
         if self.player.rect.colliderect(self.portal_s3.rect):
             pygame.draw.rect(screen, (255, 0, 0), self.player.rect, 1)
             pygame.draw.rect(screen, (255, 0, 0), self.portal_s3.rect, 1)
+            if self.portal_s3.reponse==True:
+                 screen.blit(bravo, (WIDTH // 2 - 850, 20))
+            else:
+                self.player.hp -= 2
+        
         if self.player.rect.colliderect(self.portal_s4.rect):
             pygame.draw.rect(screen, (255, 0, 0), self.player.rect, 1)
             pygame.draw.rect(screen, (255, 0, 0), self.portal_s4.rect, 1)
+            if self.portal_s4.reponse==True:
+                 screen.blit(bravo, (WIDTH // 2 - 850, 20))
+            else:
+                self.player.hp -= 2
+        
         if self.player.rect.colliderect(self.portal_s5.rect):
             pygame.draw.rect(screen, (255, 0, 0), self.player.rect, 1)
             pygame.draw.rect(screen, (255, 0, 0), self.portal_s5.rect, 1)
+            if self.portal_s5.reponse==True:
+                 screen.blit(bravo, (WIDTH // 2 - 850, 20))
+            else:
+                self.player.hp -= 2
+
+            
         if self.player.rect.colliderect(self.portal_s6.rect):
             pygame.draw.rect(screen, (255, 0, 0), self.player.rect, 1)
             pygame.draw.rect(screen, (255, 0, 0), self.portal_s6.rect, 1)
-            screen.blit(bravo, (WIDTH // 2 - 850, 20))
+            if self.portal_s6.reponse==True:
+                 screen.blit(bravo, (WIDTH // 2 - 850, 20))
+            else:
+                self.player.hp -= 2
 
-
-        #self.portal_spawn()
-
-    """def portal_spawn(self):
-        portal_s1 = Portal()
-        portal_s2 = Portal(1600, 400)
-        portal_s3 = Portal(1600, 50)
-        portal_s4 = Portal(1100, 750)
-        portal_s5 = Portal(1100, 400)
-        portal_s6 = Portal(1100, 50)"""
-       # self.all_portals.add(portal_s1, portal_s2, portal_s3, portal_s4, portal_s5, portal_s6)

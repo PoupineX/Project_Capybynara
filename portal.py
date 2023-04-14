41ce6a8f4d0b2c819c5ced15e2fd311f881ef0ef
PoupineX <92035497+PoupineX@users.noreply.github.com>
2023-04-14 16:24:35 +0200

    Update portal.py
    V2

--- portal.py
+++ portal.py
@@ -4,10 +4,10 @@ import pygame
 class Portal(pygame.sprite.Sprite):
 
     pos = [(1600, 810), (1600, 450), (1600, 90), (1100, 810), (1100, 450), (1100, 90)]
-    def __init__(self, x, y):
+    def __init__(self, x, y, bonne_reponse):
         super().__init__()
         self.image = pygame.image.load("assets_NSI/portal_s.png")
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        #self.val = eliot.nbs_atr()
+        self.reponse = bonne_reponse
